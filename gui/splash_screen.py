861d37f1d9a1b0f449ecfc6f61edf4a42b621550
ExDCode <exdcode@gmail.com>
2024-06-04 04:10:39 -0700

    GUI: Adjusted number of rows for SplashScreen and textwrap for txt_underlogo

--- gui/splash_screen.py
+++ gui/splash_screen.py
@@ -28,7 +28,7 @@ class SplashScreen(tk.Frame):
         background_label = tk.Label(foreground_frame, image=photo, borderwidth=0)
         background_label.image = photo
         #background_label.place(relx=0.5, rely=0.5, anchor="center")
-        background_label.grid(row=1, column=1, sticky="sew")
+        background_label.grid(row=1, column=1, rowspan=3, sticky="nsew")
         background_label.configure(bg='white')
         # Bind the resize event to the resize_image function
         #self.bind("<Configure>", resize_image)
@@ -38,15 +38,17 @@ class SplashScreen(tk.Frame):
         foreground_frame.grid_columnconfigure(1, weight=5)
         foreground_frame.grid_columnconfigure(2, weight=1)
         foreground_frame.grid_rowconfigure(0, weight=1)
-        foreground_frame.grid_rowconfigure(1, weight=2)
+        foreground_frame.grid_rowconfigure(1, weight=1)
         foreground_frame.grid_rowconfigure(2, weight=1)
+        foreground_frame.grid_rowconfigure(3, weight=1)
+        foreground_frame.grid_rowconfigure(4, weight=1)
 
         # Load the Top Right Corner image
         photo = ImageTk.PhotoImage(Image.open(image_path+"top_right_sm.png"))
         img_corner_topright = tk.Label(foreground_frame, image=photo, borderwidth=0)
         img_corner_topright.image = photo
         img_corner_topright.place(x=0, y=0, relwidth=1, relheight=1)
-        img_corner_topright.grid(row=0,column=2,padx=5, pady=5, sticky="ne")
+        img_corner_topright.grid(row=0,column=2,rowspan=3, padx=5, pady=5, sticky="ne")
         # Bind the resize event to the resize_image function
         #self.bind("<Configure>", resize_image)
 
@@ -55,7 +57,7 @@ class SplashScreen(tk.Frame):
         img_corner_bottomleft = tk.Label(foreground_frame, image=photo, borderwidth=0)
         img_corner_bottomleft.image = photo
         img_corner_bottomleft.place(x=0, y=0, relwidth=1, relheight=1)
-        img_corner_bottomleft.grid(row=2,column=0,padx=5, pady=5, sticky="sw")
+        img_corner_bottomleft.grid(row=2,column=0, rowspan=3, padx=5, pady=5, sticky="sw")
         # Bind the resize event to the resize_image function
         #self.bind("<Configure>", resize_image)
 
@@ -65,7 +67,7 @@ class SplashScreen(tk.Frame):
         btn_open.image = photo
         #btn_open.place(x=0, y=0, relwidth=1, relheight=1)
         btn_open.bind('<Button-1>', self.openButton)
-        btn_open.grid(row=2,column=1,padx=5, pady=5)
+        btn_open.grid(row=4,column=1,padx=5, pady=5)
         btn_open.configure(bg='white')
         # Bind the resize event to the resize_image function
         #self.bind("<Configure>", resize_image)
@@ -74,13 +76,13 @@ class SplashScreen(tk.Frame):
         photo = ImageTk.PhotoImage(Image.open(image_path+"Logo_sm.png"))
         img_logo = tk.Label(foreground_frame, image=photo, borderwidth=0)
         img_logo.image = photo
-        img_logo.grid(row=2,column=2,padx=5, pady=20, sticky="se")
+        img_logo.grid(row=4,column=2,padx=5, pady=35, sticky="se")
         img_logo.configure(bg='white')
         # Bind the resize event to the resize_image function
         #self.bind("<Configure>", resize_image)
 
         # Load the EXIT button
-        photo = ImageTk.PhotoImage(Image.open(image_path+"btn-exit.png"))
+        photo = ImageTk.PhotoImage(Image.open(image_path+"btn-exit_sm.png"))
         btn_exit = tk.Label(foreground_frame, image=photo )
         btn_exit.image = photo
         #btn_exit.place(x=0, y=0, relwidth=1, relheight=1)
@@ -92,13 +94,15 @@ class SplashScreen(tk.Frame):
         # Write text for under the LOGO
         txt_underlogo= tk.Label(foreground_frame,
                                  text = "Product of SkyHarvest Insights",
-                                 font = ("Montserrat", 10))
-        txt_underlogo.grid(row=2,column=2,padx=5, pady=5, sticky="se")
+                                 font = ("Montserrat", 10),
+                                 wraplength=140,
+                                 justify="right")
+        txt_underlogo.grid(row=4,column=2,padx=5, pady=5, sticky="se")
         txt_underlogo.configure(bg='white')
 
         # Write text for Version number
         txt_version = tk.Label(foreground_frame, text = "Version 1.00")
-        txt_version.grid(row=2,column=1,padx=5, pady=5, sticky="s")
+        txt_version.grid(row=4,column=1,padx=5, pady=5, sticky="s")
         txt_version.configure(bg='white')
 
 
