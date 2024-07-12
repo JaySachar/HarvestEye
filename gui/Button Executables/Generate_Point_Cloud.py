f29b9eb2d31f335870fa51a7ed3f57965eaa94de
JaySachar <90127501+JaySachar@users.noreply.github.com>
2024-07-11 19:53:34 -0700

    add general functions for buttons

---
+++ gui/Button Executables/Generate_Point_Cloud.py
@@ -0,0 +1,6 @@
+"""
+A script for taking the image directory selected by the user
+and creating a Point Cloud using OpenMVG.
+PointCloudGenerator method generatePointCloud()
+Called when the "Submit" button in data_entry_screen.py is clicked. 
+"""
---
+++ gui/Button Executables/height_and_volume_analysis_script.py
@@ -0,0 +1,9 @@
+"""
+A script for taking the Point Cloud generated as input, and 
+doing KMeans, Filtering, and then DBSCAN. Analysis is then done on this.
+Code taken from test2.py.
+
+1 input is the point cloud
+1 input is the global variable (ie. "Chili", "Orange", "Hazelnut") giving different predefined weights.
+"""
+
